ea464243ba2700add00e45342975b161e0c29554
mitch lindsey <mlindsey19@gmail.com>
2018-11-26 12:23:14 -0600

    delete, recheck for position to print, sets parent pointers

--- src/algorithms/binarySearchTree/BinarySearchTree.kt
+++ src/algorithms/binarySearchTree/BinarySearchTree.kt
@@ -4,7 +4,7 @@ import java.lang.StringBuilder
 import kotlin.math.pow
 
 data class Node(var value: Int, var position: StringBuilder = StringBuilder(), var leftNode: Node? = null,
-                var rightNode: Node? = null, var parentNode: Node? = null, var depth: Int = 0) {}
+                var rightNode: Node? = null, var parentNode: Node? = null, var depth: Int = 0)
 
 class BinarySearchTree {
 
@@ -21,7 +21,7 @@ class BinarySearchTree {
         while (node != null) {
             currentNode = node
             node = if ( newNode.value < node.value) node.leftNode.also {  newNode.position.append(0) }
-            else node.rightNode.also {newNode.position.append(1) }
+            else node.rightNode.also {newNode.position.append(1) } //append to string for printing position
         }
 
         newNode.parentNode = currentNode
@@ -41,7 +41,7 @@ class BinarySearchTree {
             height = newNode.depth
         }
 
-        treeMap.put(decimalPos(newNode).toInt(), newNode.value!!.toInt())
+        treeMap[decimalPos(newNode).toInt()] = newNode.value
     }
 
     fun inOrder(node: Node){
@@ -53,14 +53,14 @@ class BinarySearchTree {
         println(node.value)
         node.leftNode?.let { preOrder(node.leftNode!!) }
         node.rightNode?.let { preOrder(node.rightNode!!) }
-    }fun postOrder(node: Node){
+    }
+    fun postOrder(node: Node){
         node.leftNode?.let { postOrder(node.leftNode!!) }
         node.rightNode?.let { postOrder(node.rightNode!!) }
         println(node.value)
-
     }
 
-    fun minimum(node: Node): Node {
+    private fun minimum(node: Node): Node {
         node.leftNode?.let { return minimum( node.leftNode!! ) }
         return node
     }
@@ -68,29 +68,40 @@ class BinarySearchTree {
         node.rightNode?.let { return maximum( node.rightNode!! ) }
         return node
     }
-
     private fun successor(node: Node): Node? {
         node.rightNode?.let { return minimum(node.rightNode!!) }
         val parentNode = node.parentNode
         return if (node == parentNode?.rightNode) successor(node.parentNode!!) else parentNode
     }
-    fun delete (node: Node) {
-
-        // set successor children to node children         //and set successor child to left-child of parent
-        node.rightNode?.let {
-            successor(node)!!.rightNode = node.rightNode.also {
-                successor(node)!!.leftNode = node.leftNode.also {
-                    successor(node)?.parentNode?.leftNode = successor(node)?.leftNode
-                    return
+    fun delete (num: Int) {
+        val node: Node? = query(num)
+        node?.let {
+            // set successor children to node children         //and set successor child to left-child of parent
+            node.rightNode?.let {
+                val successor = successor(node)
+                successor!!.rightNode = node.rightNode.also {
+                    successor.leftNode = node.leftNode.also {
+                        successor.parentNode?.leftNode = successor.leftNode
+                        if (node.value < node.parentNode!!.value) node.parentNode!!.leftNode = successor
+                        else node.parentNode!!.rightNode = successor
+                        successor.parentNode = node.parentNode
+                    }
                 }
+                treeMap.clear()
+                transverseNewPos(rootNode)
+                return
             }
+            //else right-child-node has null left-child link new node.right to node.parent.right
+            node.parentNode!!.rightNode = node.rightNode
+            node.rightNode!!.parentNode = node.parentNode
+            treeMap.clear()
+            transverseNewPos(rootNode)
         }
-        //else right-child-node has null left-child link new node.right to node.parent.right
-        node.parentNode!!.rightNode = node.rightNode
     }
 
-    fun decimalPos (node: Node): Long {
+    private fun decimalPos (node: Node): Long {
         val depth = node.position.length
+        if (depth == 0) return 0
         val posInRow = toDecimal(node.position.toString())
 
         return ( (2.0).pow(depth) + posInRow - 1 ).toLong()
@@ -114,32 +125,94 @@ class BinarySearchTree {
         var a: Int
         var b: Int
         var c: Int
-        var temp: String
-        println(this.rootNode!!.value.toString().padStart( ( ( 3 * height ) + 5 )  ) )
+        println(this.rootNode!!.value.toString().padStart( ( 2 * height.pow(2)  )  ) )
 
         for ( i in 1..height ) {
-            print("  ".padEnd(3 * (height - i) + 4) )
             a = (2.0).pow(i).toInt() - 1
-            b = (((3 * (2.0).pow(i)) - 2) / 2).toInt() - 1
+            b = (((3 * (2.0).pow(i) ) - 2) / 2).toInt() - 1
             c = ((2.0).pow(i + 1) - 2).toInt()
-            for (k in a..b) {
-                temp = if (k !in treeMap.keys) "__ "
-                else treeMap.getValue(k).toString().padEnd(3)
+            val pad = 3 * ( i + 1 )
 
-                print(temp)
-            }
-            for (j in (b + 1)..c) {
-                temp = if (j !in treeMap.keys) "__ "
-                else treeMap.getValue(j).toString().padEnd(3)
+            print( "".padEnd( 2*( height - i ).pow(2)  ) )
+            printSlash( pad, ( c - a + 1) )
+            print( "".padEnd( 2*( height - i ).pow(2)  ) )
 
-                print(temp)
-            }
+            for (k in a..b) printNode( k, pad )
+            print("".padEnd(2 * ( height - i ) ))
+            for (j in (b + 1)..c) printNode(j, pad)
 
             println()
         }
     }
 
+    private fun printSlash(pad: Int, i: Int) {
+        var slash: Slash = Slash.LEFT
+
+        repeat(i) {
+            if (slash == Slash.LEFT) {
+                print("/".padEnd(pad))
+                slash = Slash.Right
+            }
+            else if (slash == Slash.Right){
+                print("\\".padEnd(pad))
+                slash = Slash.LEFT
+            }
+        }
+        println()
+    }
+
+    private fun printNode(k: Int, pad: Int){
+
+        val temp = if (k !in treeMap.keys) "__".padEnd( pad )
+        else treeMap.getValue(k).toString().padEnd(pad)
+        print(temp)
+    }
+
+    fun query( num: Int): Node? {
+        var node = rootNode
+        while(node != null)  {
+            if (node.value == num)
+                return node
+            if (node.value > num )
+                node = node.leftNode
+            if (node!!.value < num)
+                node = node.rightNode
+        }
+
+        return node
+    }
+
+    private fun newPosString(node: Node){
+        var tempNode = node
+        node.position.clear()
+        while (tempNode !== rootNode){
+            if (tempNode.value < tempNode.parentNode!!.value)
+                node.position.insert(0, "0")
+            else
+                node.position.insert(0, "1")
+            tempNode = tempNode.parentNode!!
+
+        }
+        treeMap[decimalPos(node).toInt()] = node.value
+    }
+
+    private fun transverseNewPos(node: Node?){
+        newPosString(node!!)
+        node.rightNode?.let { transverseNewPos(node.rightNode) }
+        node.leftNode?.let { transverseNewPos(node.leftNode) }
+    }
+
 }
+
+
+
+private fun Int.pow(i: Int): Int {
+    return this.toDouble().pow(i).toInt()
+}
+enum class Slash{ LEFT, Right }
+
+
+
 
 
 
--- src/algorithms/binarySearchTree/Menu.kt
+++ src/algorithms/binarySearchTree/Menu.kt
@@ -3,21 +3,16 @@ package algorithms.binarySearchTree
 //30, 10, 45, 38, 20, 50, 25, 33, 8, 12
 
 fun main(args : Array<String>) {
-    var tree: BinarySearchTree = BinarySearchTree()
-    val list = listOf<Int>(30, 10, 45, 38, 20, 50, 25, 33, 8, 12)
+    var tree = BinarySearchTree()
+    val list = listOf(30, 10, 45, 38, 20, 50, 25, 33, 8, 12)
 
     for (num in list)
         tree.insert(Node(num))
 
-    tree.inOrder(tree.rootNode!!)
-    println(tree.minimum(tree.rootNode!!).value)
-
-    //tree.postOrder(tree.rootNode!!)
-    //tree.preOrder((tree.rootNode!!))
-    println()
-    //tree.delete(tree.rootNode!!)
-   // tree.inOrder(tree.rootNode!!)
-
     tree.printTree()
+    tree.inOrder(tree.rootNode!!)
+    tree.delete(10)
+    tree.printTree()
+    tree.inOrder(tree.rootNode!!)
 }
 
